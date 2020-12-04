5eea1c20056737d008af721543d1890914bafaac
Asafoetida Birthday <abirthda@oa-d1.msk.21-school.ru>
2020-12-04 15:37:15 +0300

    errors

--- map/checks/checks.c
+++ map/checks/checks.c
@@ -6,7 +6,7 @@
 /*   By: abirthda <abirthda@student.21-schoo>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/04 14:56:53 by abirthda          #+#    #+#             */
-/*   Updated: 2020/12/04 15:23:32 by abirthda         ###   ########.fr       */
+/*   Updated: 2020/12/04 15:35:30 by abirthda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ t_bool		ft_is_texture(char *line)
 	return ((line[0] == 'N' && line[1] == 'O' && line[2] == ' ') ||
 			(line[0] == 'S' && line[1] == 'O' && line[2] == ' ') ||
 			(line[0] == 'W' && line[1] == 'E' && line[2] == ' ') ||
-			(line[0] == 'E' && line[1] == 'A' ** line[2] == ' '))
+			(line[0] == 'E' && line[1] == 'A' && line[2] == ' '));
 }
 
 t_bool		ft_is_sprite(char *line)
@@ -38,15 +38,4 @@ t_bool		ft_is_floor(char *line)
 t_bool		ft_is_ceilling(char *line)
 {
 	return (line[0] == 'C' && line[1] == ' ');
-}
-
-t_bool		ft_is_map(char *line)
-{
-	while (*line != '\0)
-	{
-		if (*line != '1' || *line != ' ')
-			return (0)
-		line++;
-	}
-	return (1);
 }
